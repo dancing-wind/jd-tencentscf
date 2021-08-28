6c3bfd938654dcdc1562da7f4bbd0180e0c17b1d
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-08-28 22:13:15 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 git clone -b master https://github.com/Tsukasa007/my_script.git ~/tsuk
 cd ~/tsuk
 if [ -e *.js ]；then //这里是判断语句，-e表示进行比较结果为真则存在
-echo "文件存在"
-else
-echo "文件不存在"
+   echo "文件存在"
+  else
+  echo "文件不存在"
 fi
 git clone -b wen https://github.com/Wenmoux/scripts.git ~/wen
 if [ -e ~/wen/jd/*.js ]；then //这里是判断语句，-e表示进行比较结果为真则存在
-echo "文件存在"
-else
-echo "文件不存在"
+   echo "文件存在"
+   else
+   echo "文件不存在"
 fi
