6e47ca8635cb33c83dff87c1efd9ab7d1daa1e09
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-08-28 22:16:34 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 git clone -b master https://github.com/Tsukasa007/my_script.git ~/tsuk
-if [ -e ~/tsuk/*.js ]；then //这里是判断语句，-e表示进行比较结果为真则存在
-   echo "文件存在"
+if [ ! -x ~/tsuk ]; then
+   echo "克隆成功"
   else
-  echo "文件不存在"
+  echo "克隆失败"
 fi
 git clone -b wen https://github.com/Wenmoux/scripts.git ~/wen
-if [ -e ~/wen/jd/*.js ]；then //这里是判断语句，-e表示进行比较结果为真则存在
-   echo "文件存在"
-   else
-   echo "文件不存在"
+if [ ! -x ~/jd ]; then
+   echo "克隆成功"
+  else
+  echo "克隆失败"
 fi
