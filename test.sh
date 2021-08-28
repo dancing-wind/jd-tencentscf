fec2fbee5942568c2bf058a4eb59f0474e6160f1
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-08-28 22:19:06 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 git clone -b master https://github.com/Tsukasa007/my_script.git ~/tsuk
-if [ ! -x ~/tsuk ]; then
+if [ -x ~/tsuk ]; then
    echo "克隆成功"
   else
   echo "克隆失败"
 fi
 git clone -b wen https://github.com/Wenmoux/scripts.git ~/wen
-if [ ! -x ~/jd ]; then
+if [ -x ~/jd ]; then
    echo "克隆成功"
   else
   echo "克隆失败"
