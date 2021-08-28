bc544b1eb61aa64bf457e1cfeed0a71ef8295910
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2021-08-28 22:14:17 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 git clone -b master https://github.com/Tsukasa007/my_script.git ~/tsuk
-cd ~/tsuk
-if [ -e *.js ]；then //这里是判断语句，-e表示进行比较结果为真则存在
+if [ -e ~/tsuk/*.js ]；then //这里是判断语句，-e表示进行比较结果为真则存在
    echo "文件存在"
   else
   echo "文件不存在"
